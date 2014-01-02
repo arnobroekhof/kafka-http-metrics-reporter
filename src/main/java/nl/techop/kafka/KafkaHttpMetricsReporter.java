0c80d2c31c18f8f4d048d4bb9b83398b62501f5a
Arno Broekhof <arnobroekhof@gmail.com>
2014-01-02 21:34:32 +0100

    Added Comments

--- src/main/java/nl/techop/kafka/KafkaHttpMetricsReporter.java
+++ src/main/java/nl/techop/kafka/KafkaHttpMetricsReporter.java
@@ -1,3 +1,21 @@
+/*
+ * *
+ *  * Copyright 2014, arnobroekhof@gmail.com
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *   http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
 package nl.techop.kafka;
 
 import kafka.metrics.KafkaMetricsConfig;
@@ -6,11 +24,13 @@ import kafka.utils.VerifiableProperties;
 import org.apache.log4j.Logger;
 
 /**
- * KafkaHttpMetricsReporter
- * nl.techop.kafka
- * User: arnobroekhof
- * Date: 31-12-13
- * Time: 14:18
+ * Class KafkaHttpMetricsReporter
+ * <p/>
+ * Author: arnobroekhof
+ * <p/>
+ * Purpose: Main class that is being called by Kafka on startup. This Class is also repsonsible for looking up the
+ * metric settings as configured in the kafka server.properties file en based on those settings it starts the
+ * embedded Jetty Server with the CodaStale servlets attached to it.
  */
 public class KafkaHttpMetricsReporter implements KafkaMetricsReporter, KafkaHttpMetricsReporterMBean {
 
@@ -28,27 +48,39 @@ public class KafkaHttpMetricsReporter implements KafkaMetricsReporter, KafkaHttp
   private int port = DEFAULT_PORT;
 
 
+  /*
+   * {@inheritDoc}
+   */
   @Override
   public void init(VerifiableProperties verifiableProperties) {
+
     if (!initialized) {
+      // get configured metrics from kafka
       KafkaMetricsConfig metricsConfig = new KafkaMetricsConfig(verifiableProperties);
 
+      // get the configured properties from kafka to set the bindAddress and port.
       bindAddress = verifiableProperties.getProperty("kafka.http.metrics.host");
       port = Integer.parseInt(verifiableProperties.getProperty("kafka.http.metrics.port"));
       enabled = Boolean.parseBoolean(verifiableProperties.getProperty("kafka.http.metrics.reporter.enabled"));
 
+      // construct the Metrics Server
       metricsServer = new KafkaHttpMetricsServer(bindAddress, port);
       initialized = true;
 
+      // call the method startReporter
       startReporter(metricsConfig.pollingIntervalSecs());
     } else {
       LOG.error("Kafka Http Metrics Reporter already initialized");
     }
   }
 
+  /*
+   * {@inheritDoc}
+   */
   @Override
   public synchronized void startReporter(long pollingPeriodSecs) {
     if (initialized && !running && enabled) {
+      // start the metrics server
       metricsServer.start();
       running = true;
     } else {
@@ -60,13 +92,20 @@ public class KafkaHttpMetricsReporter implements KafkaMetricsReporter, KafkaHttp
     }
   }
 
+  /*
+   * {@inheritDoc}
+   */
   @Override
   public synchronized void stopReporter() {
     if (initialized && running) {
+      // stop the metrics server
       metricsServer.stop();
     }
   }
 
+  /*
+   * {@inheritDoc}
+   */
   @Override
   public String getMBeanName() {
     return "kafka:type=nl.techop.kafka.KafkaHttpMetricsReporter";
